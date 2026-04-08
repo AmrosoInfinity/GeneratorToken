ad0b45f958c792c90f87179f828af14877a68cf0
Amateur Root Solution <49888553+AmrosoInfinity@users.noreply.github.com>
2026-04-08 16:14:33 +0700

    Update token_validate_utils.py

--- utils/token_validate_utils.py
+++ utils/token_validate_utils.py
@@ -181,7 +181,7 @@ def validate_token(token: str) -> tuple[bool, str]:
             name = data.get("name", "")
 
             # Tentukan sumber berdasarkan nilai name
-            if name == "akun inject doang":
+            if name == "akun inject doang" && "Stephie":
                 source_info = CHECKTOKEN_SOURCE_AMROSOL_MSG
             else:
                 source_info = CHECKTOKEN_SOURCE_EXTERNAL_MSG.format(name=name)
